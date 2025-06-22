5e268987ead87dc0758e230b61161774eb4d6d67
Ori Apel <oriapel12@gmail.com>
2025-06-22 18:12:01 +0300

    Refactored the code

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,63 @@
-mod calculator;
-
+//! This is a simple program that get numbers and action from the user,
+//! and perform the wanted calculation accordingly
 use std::io::stdin;
+use rusty::calculator::do_math;
 
-/// This function reads a number from the user, and make sure that it is a valid number.
-fn get_number_from_user() -> f64 {
+/// Reads a f64 number from the user input
+fn get_f64_from_user() -> Result<f64, Box<dyn std::error::Error>> {
     let mut input = String::new();
-    stdin()
-        .read_line(&mut input)
-        .expect("Failed to read from stdin :(");
-    input.trim().parse().expect("Input is not a number!")
+
+    stdin().read_line(&mut input)?;
+
+    let number = input.trim().parse::<f64>()?;
+
+    Ok(number)
 }
 
-/// This function reads mathematic action from the user
-fn get_action_from_user() -> String {
-    let mut action = String::new();
-    stdin()
-        .read_line(&mut action)
-        .expect("Failed to read from stdin :(");
-    action.trim().to_string()
+/// Reads a string from the user input
+fn get_text_from_user() -> Result<String, Box<dyn std::error::Error>> {
+    let mut input = String::new();
+
+    stdin().read_line(&mut input)?;
+
+    Ok(input.trim().to_string())
 }
 
 fn main() {
     println!("Welcome to the ~cyber~ calculator!");
 
     println!("Please enter your first number:");
-    let first_number = get_number_from_user();
+    let first_number = match get_f64_from_user() {
+                Ok(num) => num,
+                Err(e) => {
+                    println!("Error: {e}");
+                    return;
+                }
+            };
 
     println!("Please enter your second number:");
-    let second_number = get_number_from_user();
+
+    let second_number = match get_f64_from_user() {
+                Ok(num) => num,
+                Err(e) => {
+                    println!("Error: {e}");
+                    return;
+                }
+            };
 
     println!("Enter operation (+, -, *, /):");
-    let operation = get_action_from_user();
+    let operation = match get_text_from_user() {
+                        Ok(text) => text,
+                        Err(e) => {
+                            println!("Error reading text: {e}");
+                            return;
+                        }
+                    };
 
-    let mut calculator_answer = calculator::CalculatorAnswer::new();
+    match do_math(first_number, second_number, &operation) {
+        Ok(ans) => println!("answer is {ans}"),
+        Err(e) => println!("Error! {e}"),
+    }
 
-    calculator_answer.do_math(first_number, second_number, &operation);
-    println!("{}", calculator_answer);
     println!("Thank you for using the ~cyber~ calculator!");
 }
